6b2019367e6e82b5a90bc23875a83ffbb5556c36
Viktorija <viktorija_1102992@yahoo.com>
2018-11-28 10:33:50 +0100

    Add header components and scss styles

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'; //mora da go vovedeme
+
+@Component({//metadata
+ selector: "bwm-header", //ime po koe kje se selektira elementot
+ templateUrl: "./header.component.html", //posocuvame kade e html
+ styleUrls: ["./header.component.scss"]
+})
+export class HeaderComponent{ //definirame class i pishee export da e gotov za 
+                             //koristenje vo drug fajl
+
+}
