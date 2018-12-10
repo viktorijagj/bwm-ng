668ee04e42d9c597bf6b107e5e93a915ab82b107
Viktorija <viktorija_1102992@yahoo.com>
2018-12-10 12:11:15 +0100

    Adds rental list component,adds rental component, add rental detail component, add rental service and rental model

---
+++ src/app/rental/rental.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+
+
+import { RentalListComponent } from './rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental-list-item/rental-list-item.component'; 
+import { RentalComponent } from './rental.component';
+import {CommonModule} from '@angular/common';
+
+import{ RentalService } from './shared/rental.service';
+import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+
+const routes: Routes = [
+    {path:'rentals', component: RentalComponent,
+    children: [
+        {path:'' , component: RentalListComponent},
+        {path:':rentalId' , component: RentalDetailComponent}
+    ]
+},
+  
+  ]
+
+@NgModule({
+    declarations: [
+    RentalListComponent,
+    RentalListItemComponent,
+    RentalComponent,
+    RentalDetailComponent
+    
+    ],
+    imports:[CommonModule,
+            RouterModule.forChild(routes)
+            ],
+    providers: [RentalService]
+})
+
+export class RentalModule{
+
+}
---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,16 @@
+
+
+export class Rental{
+    id: string;
+    title: string;
+    city: string;
+    street: string;
+    category: string;
+    image: string;
+    bedrooms: number;
+    description: string;
+    dailyrate: number;
+    shared: boolean;
+    createdAt: string;
+
+}
